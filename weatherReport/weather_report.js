5ba4fccf002fc8a5c3c01257922d371af2fb89df
Nazar <nnakapeliukh@gmail.com>
2024-03-26 10:41:28 -0400

    Adds lon and lat  for weather select

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -22,4 +22,29 @@ function showweatherDetails(event) {
           });
 }
 
+function showLatWeatherDetails(event) {
+    event.preventDefault();
+
+    const lat = parseFloat(document.getElementById('lat').value);
+    const lon = parseFloat(document.getElementById('lon').value);
+    console.log("CIIIIITTTTTYYYYYYY: ", city);
+    const apiKey = '70edf50531a2dda86048c3a1f2bd5e3c'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            console.log(data);
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                          <p>Temperature: ${data.main.temp} &#8451;</p>
+                          <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+            console.error('Error fetching weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+          });
+}
 document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
+document.getElementById('latWeatherForm').addEventListener('submit', showLatWeatherDetails);
